de5a690eaf426d615985b8fdab5d1940406f5bf5
Frank Boccia <Mrbochiballs1@Franks-MBP.home>
2017-03-09 15:23:08 -0500

    First commit

---
+++ CollectionViewTESST/ChatCell.swift
@@ -0,0 +1,9 @@
+//
+//  ChatCell.swift
+//  CollectionViewTESST
+//
+//  Created by Frank Joseph Boccia on 3/9/17.
+//  Copyright © 2017 Frank Joseph Boccia. All rights reserved.
+//
+
+import Foundation
